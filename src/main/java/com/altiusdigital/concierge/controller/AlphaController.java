8a75c24bf95804e8c816ebc5029934d42d45b417
ranjan rajendran <ranjan.rajendran@gmail.com>
2022-07-03 11:21:22 -0400

    RANJAN LOCAL commit for second sop

--- src/main/java/com/altiusdigital/concierge/controller/AlphaController.java
+++ src/main/java/com/altiusdigital/concierge/controller/AlphaController.java
@@ -4,6 +4,6 @@ public class AlphaController {
 
     public static void main(String[] args) {
         System.out.println("Hello AlphaController");
-        System.out.println("Hello AlphaController SECOND MEESAGE");
+        System.out.println("Hello AlphaController SECOND MEESAGE 123");
     }
 }
